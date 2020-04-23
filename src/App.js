dbe4d69bfd7f087f4604c7308b061c2d91337f2a
Jonathanh7 <Jonathanh949@gmail.com>
2020-04-23 01:05:30 -0400

    finished about me and project section

--- src/App.js
+++ src/App.js
@@ -8,45 +8,84 @@ function App() {
     <div className="App">
       <Header />
       <section>
-        <h1>Hi I'm Jonathan</h1>
+        <h1 className="animated infinite bounce delay-2s">Hi I'm Jonathan</h1>
         <h2>A Friendly Full-Stack Developer</h2>
       </section>
 
       <section id={"projects"}>
         <h1 className="time">Projects</h1>
         <div className="pics">
-          <Link className="wyze">
-          <img
-            onClick={() => {
-              window.open("https://wyze-one.netlify.app/");
-            }} 
-            src="https://i.imgur.com/DY8ndyzm.png"
-          />
-          </Link>
-          <Link className="wyze">
+          <Link>
             <img
+              className="wyze animated infinite pulse"
+              onClick={() => {
+                window.open("https://wyze-one.netlify.app/");
+              }}
+              src="https://i.imgur.com/DY8ndyzm.png"
+            />
+            <p>Wyze: an app full of advice.</p>
+            <i
+              onClick={() => {
+                window.open("https://github.com/Jonathanh7/wyze-react");
+              }}
+              class="fab fa-github"
+            ></i>
+          </Link>
+
+          <Link>
+            <img
+              className="wyze animated infinite pulse"
               onClick={() => {
                 window.open("http://moonman.surge.sh/");
               }}
               src="https://i.imgur.com/4Y19dpcm.png"
             />
+            <p>moonMen: Build your spaceship.</p>
+            <i
+              onClick={() => {
+                window.open("https://github.com/rderosa1/moonMan_app");
+              }}
+              class="fab fa-github"
+            ></i>
           </Link>
-          
-          <Link className="wyze">
+
+          <Link>
             <img
-               onClick={() => {
+              className="wyze animated infinite pulse"
+              onClick={() => {
                 window.open("http://volcanic.surge.sh/");
               }}
-            src="https://i.imgur.com/vU84Lmqm.png"
-          />
+              src="https://i.imgur.com/vU84Lmqm.png"
+            />
+            <p>Volcanic Escapades:</p>
+            <p>A volcano hiking blog.</p>
+            <i
+              onClick={() => {
+                window.open("https://github.com/Jonathanh7/Volcanoes");
+              }}
+              class="fab fa-github"
+            ></i>
           </Link>
         </div>
       </section>
 
       <section id={"about"}>
         <h1>About Me</h1>
-        <p></p>
-        <img src="" />
+        <p className="info">
+          I'm a Full-Stack Developer with a background in sales, who has a
+          fervor for working on innovative ideas. As my prior experiences shows,
+          obtaining results whilst bringing new ideas is a continuous interest
+          of mine. Always happy to meet new people from all backgrounds as well
+          as working in team environments.{" "}
+        </p>
+        <div className="icons">
+        <i class="fab fa-js-square fa-2x animated infinite pulse"></i>
+        <i class="fab fa-html5 fa-2x animated infinite pulse"></i>
+        <i class="fab fa-css3 fa-2x animated infinite pulse"></i>
+        <i class="fab fa-react fa-2x animated infinite pulse"></i>
+        <i class="fas fa-gem fa-2x animated infinite pulse"></i>
+        <i class="fab fa-npm fa-2x animated infinite pulse"></i>
+        </div>
       </section>
 
       <section id={"contact"}>
