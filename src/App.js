7a07b52cd4ae9d5ee041ac50487a14aa2e5cb9f3
Jonathanh7 <Jonathanh949@gmail.com>
2020-04-22 20:10:22 -0400

    added links

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { Link, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header /> 
+      <Header />
       <section>
         <h1>Hi I'm Jonathan</h1>
         <h2>A Friendly Full-Stack Developer</h2>
       </section>
-      <section>
+
+      <section id={"projects"}>
         <h1 className="time">Projects</h1>
-        <img src="https://i.imgur.com/DY8ndyzm.png" />
-        <img src="https://i.imgur.com/4Y19dpcm.png" />
-        <img src="https://i.imgur.com/vU84Lmqm.png" />
+        <div className="pics">
+          <Link className="wyze">
+          <img
+            onClick={() => {
+              window.open("https://wyze-one.netlify.app/");
+            }} 
+            src="https://i.imgur.com/DY8ndyzm.png"
+          />
+          </Link>
+          <Link className="wyze">
+            <img
+              onClick={() => {
+                window.open("http://moonman.surge.sh/");
+              }}
+              src="https://i.imgur.com/4Y19dpcm.png"
+            />
+          </Link>
+          
+          <Link className="wyze">
+            <img
+               onClick={() => {
+                window.open("http://volcanic.surge.sh/");
+              }}
+            src="https://i.imgur.com/vU84Lmqm.png"
+          />
+          </Link>
+        </div>
       </section>
-      <section>
+
+      <section id={"about"}>
         <h1>About Me</h1>
+        <p></p>
         <img src="" />
       </section>
-      <section>
+
+      <section id={"contact"}>
         <h1>Contact Me</h1>
       </section>
-      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1 className="title">Jonathan's Website</h1>
+        <ul>
+          <a href="#projects"><li>Projects</li></a>
+         <a href="#about"><li>About Me</li></a>
+         <a href="#contact"><li>Contact</li></a>
+        </ul>
     </header>
     )
   }
